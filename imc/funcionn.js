975cf6eef23220bc9dd5466cfc6d482496ad9ec1
Gonzalezzzoe540 <gonzalezagostina540@gmail.com>
2024-12-05 18:32:17 -0300

    Add files via upload

---
+++ imc/funcionn.js
@@ -0,0 +1,20 @@
+function calcular_imc(event) {
+    event.preventDefault();
+    var altura;
+    var peso;
+    altura = document.getElementById("altura").value;
+    peso = document.getElementById("peso").value;
+    zap = document.getElementById("img_peso");
+    imc = peso/(altura*altura);
+    document.getElementById("resu").innerText = "imc" +
+    imc.toFixed(2);
+
+    if (imc < 18.5) {
+        zap.setAttribute("src", "imcbajo.png");
+    }else if (imc > 18.5 && imc < 24.9)   {
+        zap.setAttribute("src", "imcnormal.png");
+    } else 
+    {
+        zap.setAttribute("src", "imcalto.png") ;
+     }
+}
